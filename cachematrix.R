eb6667b1cf78a060f18177dd43ee4f35b9e04e43
Chris <chris.mcclure82@gmail.com>
2022-07-25 16:56:06 +1200

    R code for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix is a function that creates a special matrix object that caches the inverse of a matrix
+##  From the example code provided I have updated the matrix inverse object name to 'm' and updated each reference
+##  of 'mean' to 'inverse'
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+    m <- NULL
+    set <- function(y) {
+      x <<- y 
+      m <<- NULL
+    }
+    get <- function() x 
+    setinverse <- function(inverse) m <<- inverse  
+    getinverse <- function() m 
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Again using the example code provided I have changed 'inv' to 'm' and updated each reference of
+## 'mean' to 'inverse'
+## I have also updated the message to  say 'getting inversed matrix'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting inversed matrix")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
